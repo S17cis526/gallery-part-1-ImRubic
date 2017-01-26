2f0183f6add5770de9f91822266683208b03756d
ImRubic <Rubic@ksu.edu>
2017-01-26 16:57:24 -0600

    fixed

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
  */
  var http = require('http');
  var fs = require('fs');
- var port = 3000;
+ var port = 3100;
  
  var chess = fs.readFileSync('images/chess.jpg');
  var fern = fs.readFileSync('images/fern.jpg');
@@ -54,4 +54,59 @@
 		 res.end();
 	}
 
+ });
+ 
+ "use strict";
+
+/**
+ * server.js
+ * This file defines the server for a
+ * simple photo gallery web app.
+ */
+ var http = require('http');
+ var fs = require('fs');
+ var port = 3000;
+ 
+ function serveImage(filename, req, res) {
+	var body = fs.readFile('images/' + filename, function(err, body){
+		if(err) {
+			console.error(err);
+			res.statusCode = 500;
+			res.statusMessage = "whoops";
+			res.end("Silly me");
+			return;
+		}
+	res.setHeader("Content-Type", "image/jpeg");
+	res.end(body); 
+	});
+ }
+ 
+ 
+ var server = http.createServer(function(req, res) {
+	 
+ switch(req.url){
+	 case "/chess":
+		 serveImage('chess.jpg', req, res)
+		 break;
+	 case "/fern":
+		 serveImage('fern.jpg', req, res)
+		 break;
+	 case "/mobile":
+		 serveImage('mobile.jpg', req, res)
+		 break;
+	 case "/ace":
+		 serveImage('ace.jpg', req, res)
+		 break;
+	 case "/bubble":
+		 serveImage('bubble.jpg', req, res)
+		 break;
+	 default:
+		 res.statusCode = 404;
+		 resstatusMessage = "Not found";
+		 res.end();
+	}
+ });
+ 
+  server.listen(port, function(){
+	console.log("Listening on Port " + port);
  });
