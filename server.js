e46577291180d90a7155b4805f6ff5c4bbdeeaf2
ImRubic <Rubic@ksu.edu>
2017-01-31 13:11:24 -0600

    Fixed duplicate

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-"use strict";
+ "use strict";
 
 /**
  * server.js
@@ -9,66 +9,8 @@
  var fs = require('fs');
  var port = 3100;
  
- var chess = fs.readFileSync('images/chess.jpg');
- var fern = fs.readFileSync('images/fern.jpg');
- 
  function serveImage(filename, req, res) {
 	fs.readFile('images/' + filename, function(err, body){
-		if(err) {
-			console.error(err);
-			res.statusCode = 500;
-			res.statusMessage = "whoops";
-			res.end("Silly me");
-			return;
-		}
-	res.setHeader("Content-Type", "image/jpeg");
-	res.end(body); 
-	});
- }
- 
- 
- var server = http.createServer(function(req, res) {
-	 
- switch(req.url){
-	 case "/chess":
-		 serveImage('chess.jpg', req, res)
-		 break;
-	 case "/fern":
-	 case "/fern/":
-	 case "/fern.jpg":
-	 case "/fern.jpeg":
-		 serveImage('fern.jpg', req, res)
-		 break;
-	 case "/mobile":
-		 serveImage('mobile.jpg', req, res)
-		 break;
-	 case "/ace":
-		 serveImage('ace.jpg', req, res)
-		 break;
-	 case "/bubble":
-		 serveImage('bubble.jpg', req, res)
-		 break;
-	 default:
-		 res.statusCode = 404;
-		 resstatusMessage = "Not found";
-		 res.end();
-	}
-
- });
- 
- "use strict";
-
-/**
- * server.js
- * This file defines the server for a
- * simple photo gallery web app.
- */
- var http = require('http');
- var fs = require('fs');
- var port = 3000;
- 
- function serveImage(filename, req, res) {
-	var body = fs.readFile('images/' + filename, function(err, body){
 		if(err) {
 			console.error(err);
 			res.statusCode = 500;
